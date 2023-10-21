cc00bdac63dd4812e6dc210dd3eb8ab106219246
Pushkar155 <96383324+Pushkar155@users.noreply.github.com>
2023-10-21 23:57:19 +0530

    add: Questions Component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 import Home from './components/home/Home';
 import Unit from './components/UnitPage/Unit';
 import Questions from './components/Questions/Questions';
+import { AuthProvider } from './AuthContext';
 import {Route,Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <AuthProvider>
+      <div className="App">
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/unit" element={<Unit/>}/>
         <Route exact path="/ques" element={<Questions/>}/>
       </Routes>
     </div>
+    </AuthProvider>
+    
   );
 }
 
